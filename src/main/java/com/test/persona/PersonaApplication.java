2858c078c0fff662b76d5d6bd630cbf0af0b3dab
HugoBernier <cyril.carlier2@gmail.com>
2025-11-16 14:00:30 +0100

    build: SpringBoot init

---
+++ src/main/java/com/test/persona/PersonaApplication.java
@@ -0,0 +1,17 @@
+package com.test.persona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PersonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaApplication.class, args);
+	}
+
+}
